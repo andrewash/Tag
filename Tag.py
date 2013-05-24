2bf7327b26ac518418cc18a6afa25d0f877e6ddc
Andrew Ash <aash@zenmensch.com>
2013-05-24 17:25:29 -0400

    Commented out some lines to make it parse <script> and <style> tags

--- Tag.py
+++ Tag.py
@@ -98,31 +98,34 @@ class Tag():
 		for comment in unparseable:
 			if comment[:3] == '\n#' or comment[:3] == ' #':
 				content = content.replace(comment, (len(comment))*'.')
-
-		# script
-		unparseable = content.split('<script')
-		content = unparseable.pop(0)
-		l = len(unparseable)
-		i = 0
-		while i < l:
-			tmp = unparseable[i].split('</script>')
-			content += '.......'
-			content += len(tmp.pop(0))*'.'
-			content += '.........'
-			content += ".........".join(tmp)
-			i += 1
-
-		# style
-		unparseable = content.split('<style')
-		content = unparseable.pop(0)
-		l = len(unparseable)
-		i = 0
-		while i < l:
-			tmp = unparseable[i].split('</style>')
-			content += '......'
-			content += len(tmp.pop(0))*'.'
-			content += '........'
-			content += "........".join(tmp)
-			i += 1
-
-		return content
+        
+# Comment these lines to enable closing <style> and <script> tags
+#   useful for Ember.JS
+# https://gist.github.com/paultreny/5311310
+        # # script
+        # unparseable = content.split('<script')
+        # content = unparseable.pop(0)
+        # l = len(unparseable)
+        # i = 0
+        # while i < l:
+        #     tmp = unparseable[i].split('</script>')
+        #     content += '.......'
+        #     content += len(tmp.pop(0))*'.'
+        #     content += '.........'
+        #     content += ".........".join(tmp)
+        #     i += 1
+        # 
+        # # style
+        # unparseable = content.split('<style')
+        # content = unparseable.pop(0)
+        # l = len(unparseable)
+        # i = 0
+        # while i < l:
+        #     tmp = unparseable[i].split('</style>')
+        #     content += '......'
+        #     content += len(tmp.pop(0))*'.'
+        #     content += '........'
+        #     content += "........".join(tmp)
+        #     i += 1
+        # 
+        # return content
